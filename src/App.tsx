4720d2787fff96a96c6692d101c6b3265811a65f
crewd <newspaper321@nate.com>
2024-07-29 05:53:36 +0900

    feat: 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "./components/layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Layout>
-      <div>ddd</div>
-    </Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout>123</Layout>}>
+          메인페이지
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
